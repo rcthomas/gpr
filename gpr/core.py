46e8cecf25a2405f439636444baa9f4dc0f9c5cf
R. C. Thomas <rcthomas@lbl.gov>
2014-03-13 11:10:32 -0700

    Removed needless property methods.

--- gpr/core.py
+++ gpr/core.py
@@ -4,16 +4,8 @@ import collections
 class GaussianProcess ( object ) :
 
     def __init__( self, mean_function, covariance_function ) :
-        self._mean_function       = mean_function
-        self._covariance_function = covariance_function
-
-    @property
-    def mean_function( self ) :
-        return self._mean_function
-
-    @property
-    def covariance_function( self ) :
-        return self._covariance_function
+        self.mean_function       = mean_function
+        self.covariance_function = covariance_function
 
     @property
     def hyperparameters( self ) :
