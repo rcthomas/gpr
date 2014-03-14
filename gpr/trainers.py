0d347c259a0a56dd6faec59f104bfbe667d870f9
R. C. Thomas <rcthomas@lbl.gov>
2014-03-14 11:12:21 -0700

    Moved example.  Modified basin hopping trainer to take number of iterations as argument.  Not sure I want to continue with this trainers setup.

--- gpr/trainers.py
+++ gpr/trainers.py
@@ -6,9 +6,12 @@ import scipy.optimize
 
 from interface import Trainer
 
-# Much more work is needed here.
+# Much more work is needed here.  I am not sure I want to do it this way.
 
 class BasinHoppingTrainer ( Trainer ) :
+
+    def __init__( self, niter = 100 ) :
+        self.niter = niter
 
     def __call__( self, model ) :
         def func( hyperparameters, model ) :
@@ -16,7 +19,7 @@ class BasinHoppingTrainer ( Trainer ) :
             log_likelihood = model.log_p
             sys.stderr.write( "%s %s\r" % ( list( model.hyperparameters ), log_likelihood ) )
             return -log_likelihood
-        result = scipy.optimize.basinhopping( func, list( model.hyperparameters ), niter = 5, minimizer_kwargs = { "args" : ( model, ) } )
+        result = scipy.optimize.basinhopping( func, list( model.hyperparameters ), niter = self.niter, minimizer_kwargs = { "args" : ( model, ) } )
         sys.stderr.write( "\n" )
         model.hyperparameters = result.x
         return result
